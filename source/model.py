3c8ef9479609e7b438ec77d5c52a7a3cf187b3a3
GiannosL <giannos.louloudis@gmail.com>
2022-12-04 15:40:36 +0100

    annotated model a bit

--- source/model.py
+++ source/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -9,7 +10,7 @@ class Convolutional_Model(nn.Module):
         channels_1 = 6
         channels_2 = 16
 
-        # convolutional layer
+        # convolutional layer, input_channels refers to the image colours (1 - gray, 3 - colour)
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=channels_1, kernel_size=3, stride=1)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=channels_2, kernel_size=3, stride=1)
 
@@ -24,15 +25,23 @@ class Convolutional_Model(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, output_features)
     
-    def forward(self, x):
+    def forward(self, x) -> torch.Tensor:
+        """
+        pass the input image batch through the model
+        """
+        # pass through the convolutional layers
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, kernel_size=2, stride=2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, kernel_size=2, stride=2)
+
         # flatten images
         x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
 
+        # fully connected layers
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
+
+        # softmax -> assign decimal probabilities to images
         x = F.log_softmax(self.fc3(x), dim=1)
         return x
