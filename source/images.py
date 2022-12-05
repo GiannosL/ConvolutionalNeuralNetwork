7d675b6aee1953f7ff8149cd5243037719388bea
GiannosL <giannos.louloudis@gmail.com>
2022-12-05 15:02:23 +0100

    create a dataframe with the image information

--- source/images.py
+++ source/images.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from PIL import Image
 from glob import glob
 
 
@@ -5,7 +7,7 @@ class Images:
     def __init__(self, directory_path:str, file_type:str="jpg") -> None:
         # path to image files
         self.file_paths = self.collect_files(directory_path, file_type)
-        # images
+        # images dimensions
         self.images = self.get_images()
     
     def collect_files(self, directory_path:str, file_type:str) -> list:
@@ -20,5 +22,19 @@ class Images:
 
         return files
     
-    def get_images(self):
-        pass
+    def get_images(self) -> pd.DataFrame:
+        image_dimensions = []
+        images = []
+
+        # look through the images in your collection
+        for f in self.file_paths:
+            with Image.open(f) as img:
+                images.append(img)
+                image_dimensions.append(img.size)
+        
+        # convert list to dataframe
+        image_dim_df = pd.DataFrame(image_dimensions)
+        image_dim_df["IMG"] = images
+        image_dim_df.rename(columns={0: "X", 1: "Y"}, inplace=True)
+        
+        return image_dim_df
