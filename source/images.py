3b1f0eff6d956c86769bea0bb4b400fe1f9a57f9
GiannosL <giannos.louloudis@gmail.com>
2022-12-05 14:23:46 +0100

    .

--- source/images.py
+++ source/images.py
@@ -3,16 +3,22 @@ from glob import glob
 
 class Images:
     def __init__(self, directory_path:str, file_type:str="jpg") -> None:
-        #
+        # path to image files
         self.file_paths = self.collect_files(directory_path, file_type)
+        # images
+        self.images = self.get_images()
     
-    def collect_files(self, directory_path:str, file_type:str):
+    def collect_files(self, directory_path:str, file_type:str) -> list:
         """
         collect file paths
         """
         # check directory path format
         if directory_path.endswith("/"):
             directory_path = directory_path[:-1]
-        
+        # collect file paths
         files = glob(f"{directory_path}/*.{file_type}")
+
         return files
+    
+    def get_images(self):
+        pass
