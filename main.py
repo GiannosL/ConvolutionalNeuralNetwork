3eb9fb81a26e2fd52521ef48423b9818b05194e9
GiannosL <giannos.louloudis@gmail.com>
2022-12-16 16:26:32 +0200

    debugged model

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
-import torch.utils
-import torchvision
-
 from source.cnn import CNN
-from source.visuals import *
+from source.image_loader import Image_Loader
 
 
 # load dataset
-train_data = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=torchvision.transforms.ToTensor())
-test_data = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=torchvision.transforms.ToTensor())
-
-train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=100, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=100, shuffle=True)
+data = Image_Loader("../datasets/pet_images/")
 
 
 # convolutional model
-cnn_model = CNN(image_dimensions=28, output_classes=10)
-cnn_model.train(x=train_loader, epochs=15)
-cnn_model.predict(x=test_loader)
+cnn_model = CNN(image_dimensions=224, output_classes=2)
+cnn_model.train(x=data.train_data, epochs=5)
+cnn_model.predict(x=data.test_data)
 
 # save model
 cnn_model.save()
--- source/image_loader.py
+++ source/image_loader.py
@@ -26,7 +26,7 @@ class Image_Loader:
         test_dt = datasets.ImageFolder(f"{path}test",
                                           transform=self.test_transforms)
         # actual dataset and labels
-        self.train_data = DataLoader(train_dt, batch_size=10, shuffle=False)
-        self.train_labels = self.train_data.classes
-        self.test_data = DataLoader(test_dt, batch_size=10, shuffle=False)
-        self.test_labels = self.test_data.classes
+        self.train_data = DataLoader(train_dt, batch_size=100, shuffle=False)
+        self.train_labels = train_dt.classes
+        self.test_data = DataLoader(test_dt, batch_size=100, shuffle=False)
+        self.test_labels = test_dt.classes
