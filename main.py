3d84ced46d1b7477a948f3f2844db0fc84c44fc1
GiannosL <giannos.louloudis@gmail.com>
2022-12-19 13:21:53 +0200

    loading option

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from source.image_loader import Image_Loader
 # check input
 check = Input_Handling(yaml_path="configuration.yaml")
 
+# create CNN model
+cnn_model = CNN(image_dimensions=224, output_classes=2)
+
 if check.train_flag:
     # load dataset
     data = Image_Loader("../datasets/pet_images/")
 
     # convolutional model
-    cnn_model = CNN(image_dimensions=224, output_classes=2)
     cnn_model.train(x=data.train_data, epochs=10)
     cnn_model.predict(x=data.test_data)
 
@@ -24,4 +26,5 @@ if check.train_flag:
     cnn_model.plot_pred_loss()
 
 else:
-    pass
+    # load pre-trained model
+    cnn_model.load(model_path=check.model)
