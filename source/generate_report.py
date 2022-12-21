800d5e85c54c135d8f2d63454dace22924628fa7
GiannosL <giannos.louloudis@gmail.com>
2022-12-21 15:53:04 +0200

    number of epochs in report

--- source/generate_report.py
+++ source/generate_report.py
@@ -12,7 +12,7 @@ def read_html(filename:str) -> str:
     return my_file
 
 
-def generate_main(filename:str, output_directory:str, model_obj:Input_Handling,
+def generate_main(filename:str, output_directory:str, check_obj:Input_Handling,
                   model:CNN) -> None:
     """
     doc
@@ -23,7 +23,7 @@ def generate_main(filename:str, output_directory:str, model_obj:Input_Handling,
     # replace placeholders in string
     main_html = main_html.replace("_MODEL_NAME_", model.model_name)
     main_html = main_html.replace("_N_TRAINING_IMAGES_", "")
-    main_html = main_html.replace("_N_EPOCHS_", "")
+    main_html = main_html.replace("_N_EPOCHS_", str(check_obj.n_epochs))
 
     # save result in output directory
     with open(f"{output_directory}/main.html", "w+") as f:
@@ -35,4 +35,4 @@ def generate_report(check:Input_Handling, model:CNN, output_dir:str) -> None:
     generate HTML-report files based on templates
     """
     generate_main(filename="source/templates/home.html", output_directory=output_dir, 
-                  model_obj=check, model=model)
+                  check_obj=check, model=model)
